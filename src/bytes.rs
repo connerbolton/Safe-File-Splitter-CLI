26100535095fafcef27a7ae3c8e434ac91af8c46
connerbolton <connerbolton7@gmail.com>
2026-05-06 15:30:50 -0400

    Made formatting changes to bytes.rs

--- src/bytes.rs
+++ src/bytes.rs
@@ -9,12 +9,12 @@ pub fn size_to_bytes(size: &String) -> Result<u64> {
     let size = size.to_uppercase();
 
     let byte_multiplyer: u64 = 
-            match true {
-                _ if size.ends_with("GB") => 1073741824,
-                _ if size.ends_with("MB") => 1048576,
-                _ if size.ends_with("KB") => 1024,
-                _ => 1,
-            };
+        match true {
+            _ if size.ends_with("GB") => 1073741824,
+            _ if size.ends_with("MB") => 1048576,
+            _ if size.ends_with("KB") => 1024,
+            _ => 1,
+        };
 
     let mut byte_size: u64 = size
         .trim_end_matches(|c: char| c.is_alphabetic())
@@ -30,11 +30,11 @@ pub fn size_to_bytes(size: &String) -> Result<u64> {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub fn format_bytes(bytes: u64) -> String {
-    if bytes >= 1_073_741_824 {
+    if bytes >= 1073741824 {
         format!("{:.2} GB", bytes as f64 / 1073741824 as f64)
-    } else if bytes >= 1_048_576 {
+    } else if bytes >= 1048576 {
         format!("{:.2} MB", bytes as f64 / 1048576 as f64)
-    } else if bytes >= 1_024 {
+    } else if bytes >= 1024 {
         format!("{:.2} KB", bytes as f64 / 1024 as f64)
     } else {
         format!("{} B", bytes)
